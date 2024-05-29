d4ef35da68af62bae0da980ab6229f36dc91d250
aaron.pritzlaff <aaron@kindservices.co.uk>
2024-05-29 11:47:03 +0100

    taught admin how to update and remove products

--- shared/src/main/scala/kind/onboarding/refdata/RefDataAdmin.scala
+++ shared/src/main/scala/kind/onboarding/refdata/RefDataAdmin.scala
@@ -6,13 +6,26 @@ import zio.{Ref, Task}
   */
 trait RefDataAdmin {
   def add(product: Product): Task[Unit]
+  def update(product: Product): Task[Unit]
+  def remove(product: String): Task[Unit]
   def set(products: Seq[Product]): Task[Unit]
 }
 
 object RefDataAdmin {
 
   case class InMemory(db: Ref[Seq[Product]]) extends RefDataAdmin {
-    def add(product: Product)       = db.update(list => product +: list)
+    def add(product: Product)   = db.update(list => product +: list)
+    def remove(product: String) = db.update(_.filterNot(_.name == product))
+    def update(product: Product) = db.get.flatMap { list =>
+      list.find(_.name == product.name) match {
+        case Some(found) =>
+          for {
+            _ <- remove(product.name)
+            _ <- add(product)
+          } yield ()
+        case None => add(product)
+      }
+    }
     def set(products: Seq[Product]) = db.set(products)
   }
 
