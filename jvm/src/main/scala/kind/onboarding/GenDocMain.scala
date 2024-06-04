6196e2076f379a22fbe31c107ccae0ccc383d03e
aaron.pritzlaff <aaron@kindservices.co.uk>
2024-06-04 21:27:54 +0100

    sharing artefacts between steps

--- jvm/src/main/scala/kind/onboarding/GenDocMain.scala
+++ jvm/src/main/scala/kind/onboarding/GenDocMain.scala
@@ -1,7 +1,5 @@
 package kind.onboarding
 
-
-
 // this generates documentation in the 'docs' folder
 @main def genDocs() = {
 //  val scenarios = List(Scenario("Happy Path", 2 -> List("cheese", "pepperoni"), asMermaid, "pizza"))
